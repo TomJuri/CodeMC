2d93ff15c266f30a37b67bf75d98a1493cd889a1
TomJuri <77588297+TomJuri@users.noreply.github.com>
2021-03-22 21:21:17 +0100

    created lobby system module nothing done tho

---
+++ CodeMC-LobbySystem/src/main/java/de/tomjuri/codemc/lobbysystem/CMCLobbySystem.java
@@ -0,0 +1,16 @@
+package de.tomjuri.codemc.lobbysystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CMCLobbySystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
